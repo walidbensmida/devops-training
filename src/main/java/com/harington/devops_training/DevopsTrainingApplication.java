02ffbdfe692668bc85fb6c7382a8873f08d102b6
Walid Ben smida <walidbensmida@hotmail.fr>
2025-04-23 17:44:07 +0200

    init commit

---
+++ src/main/java/com/harington/devops_training/DevopsTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.harington.devops_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harington/devops_training/DevopsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harington.devops_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
